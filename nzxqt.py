8f77c9752c1f599c1d7e786685a46ed241d4ec34
wepiha <2267001+wepiha@users.noreply.github.com>
2018-12-22 11:53:03 +1300

    Highlights selected to selected slice Added function comments Refacted slightly

--- nzxqt.py
+++ nzxqt.py
@@ -17,9 +17,10 @@ DRIVERS = [
 ]
 
 _SLICE_BORDER = {
-    'enable'  : QtGui.QColor(1,1,1,255),
-    'disable' : QtGui.QColor(255,255,255,255),
-    'hover'   : QtGui.QColor(127,127,127,255)
+    'enable'  : QtGui.QColor(1,1,1,160),
+    'disable' : QtGui.QColor(127,127,127,160),
+    'hover'   : QtGui.QPalette().color(QtGui.QPalette.Highlight),
+    'picked'  : QtGui.QPalette().color(QtGui.QPalette.HighlightedText)
 }
 
 def find_all_supported_devices():
@@ -29,20 +30,22 @@ def find_all_supported_devices():
 class MainWindow(QtWidgets.QMainWindow):
 
     def device_selection_changed(self):
+        """Updates the interface when a device has been selected"""
         for item in self.ui.menu_Select_Device.children():
             if isinstance(item, QtWidgets.QAction):
                 item.setChecked((self.device.device.serial_number == item.objectName()))
 
         self.ui.comboBoxPresetModes.clear()
-        #self.ui.comboBoxPresetModes.addItems(self.device.get_color_modes())
+
         for mode in self.device.get_color_modes():
             self.ui.comboBoxPresetModes.addItem(str(mode).title())
 
         self.ui.labelDevDeviceName.setText("Device: %s" % self.device.description)
         self.ui.labelDevSerialNo.setText("Serial No: %s" % self.device.device.serial_number)
 
-        self.led_preset_mode_changed()
+        self.preset_mode_changed()
     def update_menu_device_list(self):
+        """Populates device menu with supported devices"""
         last_device = self.device if hasattr(self, 'device') else None
 
         self.devices = list(enumerate(find_all_supported_devices()))
@@ -63,7 +66,8 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.device = dev
 
         self.device_selection_changed()
-    def select_device_menu_tiggered(self, arg):
+    def select_device_menu_tiggered(self):
+        """Activates device menu item when clicked"""
         for i, dev in self.devices:
             if (dev.device.serial_number == self.sender().objectName()):
                 self.device = dev
@@ -71,13 +75,14 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.device_selection_changed()
 
-    def preset_save_clicked(self, value):
+    def preset_save_clicked(self):
+        """Sends preset values to the currently selected device"""
         if (self.device == None ):
             return
 
         mode = self.ui.comboBoxPresetModes.currentText().lower()
         mval, mod2, mod4, mincolors, maxcolors, ringonly = self.device.get_color_modes()[mode]
-        colors = [self.get_slice_color_by_index(0) if ringonly else bytes.fromhex(self.get_led_logo_qcolor().name().strip("#"))]
+        colors = [self.get_slice_color(0) if ringonly else bytes.fromhex(self.get_led_logo_qcolor().name().strip("#"))]
         channel = 'sync'
         speed = 'slowest'
 
@@ -89,12 +94,12 @@ class MainWindow(QtWidgets.QMainWindow):
                 break
 
         if (maxcolors > 0):
-            for i, ps in self.get_slice_enum():
+            for i, ps in enumerate(self.series.slices()):
                 if (ringonly and i==0):
                     continue
                 if (len(colors) == maxcolors):
                     break
-                colors.append(self.get_slice_color_by_index(i))
+                colors.append(self.get_slice_color(i))
 
         if (self.ui.radioButtonPresetLogo.isChecked()):
             self.ui.labelLogoMode.setText(mode)
@@ -116,21 +121,11 @@ class MainWindow(QtWidgets.QMainWindow):
             self.ui.labelBothMode.setText(mode)
 
         self.device.set_color(channel, mode.lower(), colors, speed)
-    def preset_speed_changed(self, value):
+    def preset_speed_changed(self, value: str):
+        """Updates animation speed information"""
         self.ui.labelPresetAniSpeedLabel.setText("Animation Speed: %s" % value)
-    def get_led_logo_qcolor(self):
-        color = self.ui.labelLogo.palette().color(0).name()
-        return QtGui.QColor(color)
-    def led_logo_clicked(self, evt):
-        self.picked = self.ui.labelLogo
-        self.colorDialog.setCurrentColor(self.get_led_logo_qcolor())
-    def led_mode_both_changed(self):
-        self.led_preset_mode_reselect(self.sender(), self.ui.labelBothMode)
-    def led_mode_logo_changed(self):
-        self.led_preset_mode_reselect(self.sender(), self.ui.labelLogoMode)
-    def led_mode_ring_changed(self):
-        self.led_preset_mode_reselect(self.sender(), self.ui.labelRingMode)
-    def led_preset_mode_changed(self):
+    def preset_mode_changed(self):
+        """Highlights ring segments and radiobuttons that are valid for the preset"""
         mode = self.ui.comboBoxPresetModes.currentText().lower()
         if mode == '':
             return
@@ -145,20 +140,49 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.radioButtonPresetBoth.setEnabled(not ringonly)
         self.ui.radioButtonPresetLogo.setEnabled(not ringonly)
 
-        for i, ps in self.get_slice_enum():
-            self.set_slice_border(ps, _SLICE_BORDER['enable'])
+        for i, ps in enumerate(self.series.slices()):
+            self.set_slice_border_color(ps, _SLICE_BORDER['enable'])
 
-        for i, ps in self.get_slice_enum():
+        for i, ps in enumerate(self.series.slices()):
             if i == maxcolors:
                 break
-            self.set_slice_border(ps)
-    def led_preset_mode_reselect(self, sender, target):
+            self.set_slice_border_color(ps)
+        
+        if isinstance(self.picked, QtChart.QPieSlice):
+            self.set_slice_border_color(self.picked, _SLICE_BORDER['picked'])
+        
+        font = self.ui.labelLogo.font()
+        font.setUnderline(self.picked == self.ui.labelLogo)
+        self.ui.labelLogo.setFont(font)
+    def preset_mode_reselect(self, sender: QtWidgets.QRadioButton, target: QtWidgets.QLabel):
+        """Reselects the preset based on the target value"""
         if (sender.isChecked()):
-             index = self.ui.comboBoxPresetModes.findText(target.text())
-             if index > 0:
-                 self.ui.comboBoxPresetModes.setCurrentIndex(index)
+            index = self.ui.comboBoxPresetModes.findText(target.text())
+            if index < 0:
+                index = 0
+            self.ui.comboBoxPresetModes.setCurrentIndex(index)
 
-    def led_slice_init(self):
+    def get_led_logo_qcolor(self) -> QtGui.QColor:
+        """Gets the logo QColor from its Palette"""
+        return self.ui.labelLogo.palette().color(0)
+    def led_logo_clicked(self, evt: QtGui.QMouseEvent):
+        """Set color change target to logo"""
+        self.picked = self.ui.labelLogo
+        self.colorDialog.setCurrentColor(self.get_led_logo_qcolor())
+        
+        self.preset_mode_changed()
+    def led_mode_both_changed(self):
+        """Reselects both preset when radiobutton is activated"""
+        self.preset_mode_reselect(self.sender(), self.ui.labelBothMode)
+    def led_mode_logo_changed(self):
+        """Reselects logo preset when radiobutton is activated"""
+        self.preset_mode_reselect(self.sender(), self.ui.labelLogoMode)
+    def led_mode_ring_changed(self):
+        """Reselects ring preset when radiobutton is activated"""
+        self.preset_mode_reselect(self.sender(), self.ui.labelRingMode)
+
+    def ring_init(self):
+        """Adds a ring widget as a QChart"""
         self.chart = QtChart.QChart()
         self.chartview = QtChart.QChartView(self.chart)
         self.chartview.setRenderHint(QtGui.QPainter.Antialiasing, True)
@@ -176,40 +200,46 @@ class MainWindow(QtWidgets.QMainWindow):
 
         for i in range(8):
             ps = QtChart.QPieSlice(str(i), 1)
-            #ps.setExploded(True)
-            ps.setExplodeDistanceFactor(0.025)
+            ps.setExploded(True)
+            ps.setExplodeDistanceFactor(0.035)
 
-            ps.clicked.connect(self.led_slice_clicked)
-            ps.hovered.connect(self.led_slice_hovered)
-            ps.doubleClicked.connect(self.led_slice_dblclicked)
+            ps.clicked.connect(self.ring_slice_clicked)
+            ps.hovered.connect(self.ring_slice_hovered)
+            ps.doubleClicked.connect(self.ring_slice_dblclicked)
 
             self.series.append(ps)
         self.chart.addSeries(self.series)
         self.ui.frame.setChart(self.chart)
-    def get_slice_enum(self):
-        return enumerate(self.series.slices())
-    def get_slice_color_by_index(self, index):
+    def ring_slice_clicked(self):
+        """Stores slice and sets color dialog color"""
+        self.picked = self.sender()
+        self.colorDialog.setCurrentColor(self.last_color)
+
+        self.preset_mode_changed()
+    def ring_slice_dblclicked(self):
+        """Fills all slices with the same color"""
+        for i, ps in enumerate(self.series.slices()):
+            ps.setColor(self.last_color)
+    def ring_slice_hovered(self, state):
+        """Event when slice is hovered"""
+        if state:
+            self.last_color = self.sender().color()
+            self.sender().setColor(self.last_color.lighter())
+        else:
+            self.sender().setColor(self.last_color)
+
+        self.preset_mode_changed()
+        self.sender().setExplodeDistanceFactor(0.05 if state else 0.03)
+
+        self.hovered = self.sender() if state else None
+    def get_slice_color(self, index: int) -> bytes:
+        """Returns bytes the slice at index"""
         color = self.series.slices()[index].color().name().strip("#")
         return bytes.fromhex(color)
-    def led_slice_clicked(self):
-        self.picked = self.sender()
-        self.colorDialog.setCurrentColor(self.picked.color())
-    def led_slice_dblclicked(self):
-        for i, ps in self.get_slice_enum():
-            ps.setColor(self.sender().color())
-       # color = self.get_slice_color(self.sender())
-    def led_slice_hovered(self, state):
-        if state:
-            self.slice_border = self.sender().borderColor()
-            self.set_slice_border(self.sender(), _SLICE_BORDER['hover'])
-        else:
-            self.set_slice_border(self.sender(), self.slice_border)
-        
-        self.sender().setExploded(state)
-        self.hovered = self.sender() if state else None
-    def set_slice_border(self, slice, mode = _SLICE_BORDER['disable']):
-        slice.setBorderColor(mode)
-
+    def set_slice_border_color(self, slice: QtChart.QPieSlice, mode: QtGui.QColor = _SLICE_BORDER['disable']):
+            """Set the slice border"""
+            slice.setBorderColor(mode)
+    
     def color_dialog_changed(self, value):
         if self.picked != None:
             if isinstance(self.picked, QtWidgets.QLabel):
@@ -232,11 +262,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
 
         self.picked = self.ui.labelLogo
-        self.led_slice_init()
-        self.update_menu_device_list()
+        self.ring_init()
         self.color_dialog_init()
+        self.update_menu_device_list()
 
-        self.ui.comboBoxPresetModes.currentTextChanged.connect(self.led_preset_mode_changed)
+        self.ui.comboBoxPresetModes.currentTextChanged.connect(self.preset_mode_changed)
         self.ui.pushButtonSave.clicked.connect(self.preset_save_clicked)
         self.ui.actionReload.triggered.connect(self.update_menu_device_list)
         self.ui.labelLogo.mousePressEvent = self.led_logo_clicked
