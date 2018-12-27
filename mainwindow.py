bbdaf4d1134860e8c25ac15e68fdafe8ba53ceb5
wepiha <2267001+wepiha@users.noreply.github.com>
2018-12-27 16:27:17 +1300

    Compiled mainwindow.ui

--- mainwindow.py
+++ mainwindow.py
@@ -287,7 +287,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         self.tabWidget.setCurrentIndex(1)
-        self.tabWidget_2.setCurrentIndex(1)
+        self.tabWidget_2.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
